49f7218ab861b326eb63bb66b816f232f19e73fe
Galen Casstevens <galencasstevens@gmail.com>
2019-09-05 10:09:48 -0400

    finished building the REST API in Express and Node

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
+const apiRouter = require('./app_api/routes/index');
+
 const usersRouter = require('./app_server/routes/users');
 
 const app = express();
@@ -22,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
